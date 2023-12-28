6d386d0d1b2a8917807607f4c37dbc8a6a0893c2
thamerbk <thamerbk6@gmail.com>
2023-12-28 11:58:48 +0300

    add local-storage in second way

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let inputColor2 = document.getElementById("box2");
 let linear = document.querySelector(".linear");
 
 
-// my way 1
+// first way 1(without localStorage)
 // boxs.oninput = function () {
   //   let box1 = inputColor1.value;
   //   let box2 = inputColor2.value;
@@ -14,11 +14,24 @@ let linear = document.querySelector(".linear");
   // }
   
 
-// anothor way 2
+  // second way 2(with localStorage)
+  if(window.localStorage.getItem("background")) {
+  document.body.style.background = window.localStorage.getItem("background");
+  inputColor1.value = window.localStorage.getItem("value1")
+  inputColor2.value = window.localStorage.getItem("value2")
+  linear.innerHTML = window.localStorage.getItem("text");
+}
+
   function colorChange() {
+    window.localStorage.setItem("background", `linear-gradient(to right, ${inputColor1.value}, ${inputColor2.value})`)
+    window.localStorage.setItem("value1", inputColor1.value)
+    window.localStorage.setItem("value2", inputColor2.value)
+    window.localStorage.setItem("text", document.body.style.background);
+
     document.body.style.background = `linear-gradient(to right, ${inputColor1.value}, ${inputColor2.value})`;
-  
+
     linear.innerHTML = document.body.style.background;
   }
+
   inputColor1.addEventListener("input", colorChange);
   inputColor2.addEventListener("input", colorChange);
